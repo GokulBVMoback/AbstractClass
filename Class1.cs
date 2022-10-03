63eec6e89a09f7d37247b83368517ed4bb704976
GokulBVMoback <gokulbv@moback.com>
2022-10-03 22:52:58 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public abstract class ShapeOne
+{
+    public abstract void AreaOf(int x);
+}
+public abstract class ShapeTwo
+{
+    public abstract void AreaOfRect(int x, int y);
+}
+public class Circle:ShapeOne
+{
+    public override void AreaOf(int x)
+    {
+        double area;
+        int r = x;
+        area=22 / 7 * (r * r * r) + 0;
+        Console.WriteLine("Area of the Circle:"+area);
+        //return area;
+    }
+}
+public class Square : ShapeOne
+{
+    public override void AreaOf(int x)
+    {
+        int area;
+        int l = x;
+        area=l* l;
+        Console.WriteLine("Area of the Square:"+area);
+    }
+}
+public class Rectangle : ShapeTwo
+{
+    public override void AreaOfRect(int x, int y)
+    {
+        int area;
+        int l = x;
+        int b = y;
+        area = l * b;
+        Console.WriteLine("Area of the Rectangle:"+area);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+            circle.AreaOf(4);
+            Square square = new Square();
+            square.AreaOf(5);
+            Rectangle rectangle = new Rectangle();
+            rectangle.AreaOfRect(3, 4);
+        }
+    }
+}
